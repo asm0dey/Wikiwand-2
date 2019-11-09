ff946ecc4605edcc56a55aea04bb293ee5f25c1c
asm0dey <pavel.finkelshtein@gmail.com>
2019-11-09 09:47:49 +0300

    Adds fix for mobile version to work correctly

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ function redirect(requestDetails) {
     if (requestDetails.url.endsWith("?oldformat=true")) return defaultRedirect
     var matches = regex.exec(requestDetails.url)
     if (matches[1].startsWith("Talk:")) return defaultRedirect
-    var wikiwandUrl = "https://wikiwand.com/" + matches[1] + "/" + matches[2]
+    var wikiwandUrl = "https://wikiwand.com/" + matches[1].replace('\.m','') + "/" + matches[2]
     return {
         redirectUrl: wikiwandUrl
     }
